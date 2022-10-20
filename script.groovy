df6a312535567a998680056f2b6fc0d178462653
Unbelievablee <42008315+Unbelievablee@users.noreply.github.com>
2022-10-20 20:28:42 +0600

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,19 @@
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t 127.0.0.1:8083/java-maven-app:2.2 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin 127.0.0.1:8083"
+        sh 'docker push 127.0.0.1:8083/java-maven-app:2.2'
+    }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
